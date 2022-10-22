2e7f5892ea1c69ab043b67e89fba4ada46d774fd
Christo107 <christopher.odwyer82@gmail.com>
2022-10-22 12:50:18 +0000

    Add checkWinner function to js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,17 @@ const playerChoiceImg = document.getElementById("player-choice-img");
 const computerChoiceImg = document.getElementById("computer-choice-img");
 const OPTIONS = ["rock", "paper", "scissors"]
 
+let resultMessage = document.getElementById("result-text");
+let playerOption;
+let computerOption;
+let result;
+
 /**
  * Adding event listeners to buttons
  */
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        let playerOption = this.getAttribute("data-choice");
+        playerOption = this.getAttribute("data-choice");
         playGame(playerOption);
     });
 }
@@ -26,12 +31,44 @@ function playGame(playerOption) {
     playerChoiceImg.src = `assets/images/${OPTIONS[playerOption]}.png`;
     playerChoiceImg.alt = OPTIONS[playerOption];
 
-    let computerOption = Math.floor(Math.random() * 3);
+    computerOption = Math.floor(Math.random() * 3);
 
     computerChoiceImg.src = `assets/images/${OPTIONS[computerOption]}.png`;
     computerChoiceImg.alt = OPTIONS[computerOption];
 
-    let result = checkWinner(OPTIONS[computerOption], OPTIONS[playerOption]);
-
+    checkWinner();
+    
     updateScores(result);
+}
+
+/**
+ * Checks whether player or computer has won the current round
+ */
+
+ function checkWinner() {
+     if (playerOption === "rock" && computerOption === 2) {
+         resultMessage.innerHTML = "You win! Lets have another round!";
+     }
+     else if (playerOption === "rock" && computerOption === 1) {
+         resultMessage.innerHTML = "You Lose! Lets have another round!";
+     }
+     else if (playerOption === "paper" && computerOption === 0) {
+         resultMessage.innerHTML = "You Win! Lets have another round!";
+     }     
+     else if (playerOption === "paper" && computerOption === 2) {
+         resultMessage.innerHTML = "You Lose! Lets have another round!";
+     }
+     else if (playerOption === "scissors" && computerOption === 1) {
+         resultMessage.innerHTML = "You Win! Lets have another round!";
+     } 
+     else if (playerOption === "scissors" && computerOption === 0) {
+        resultMessage.innerHTML = "You Lose! Lets have another round!";
+     } 
+     else {
+         resultMessage.innerHTML = "Its a draw! Lets have another round!";
+     }
+ }
+
+function updateScores() {
+
 }
