7dfc1a2a038601ce353a785e2fdf920ad4492b92
Christo107 <christopher.odwyer82@gmail.com>
2022-10-23 10:04:44 +0000

    Add incrementScore functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@
  */
 // Constants declaration code below based on Code Institute Rock paper Scissors game
 const buttons = document.getElementsByClassName("option-btns");
-const playerScore = document.getElementById("player-score").textContent;
-const computerScore = document.getElementById("computer-score").textContent;
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
 const playerChoiceImg = document.getElementById("player-choice-img");
 const computerChoiceImg = document.getElementById("computer-choice-img");
 const OPTIONS = ["rock", "paper", "scissors"]
@@ -51,33 +51,45 @@ function playGame(playerOption) {
  function checkWinner() {
      if (playerOption == '0' && computerOption == '2') {
         resultMessage.innerHTML = "You win! Lets have another round!";
-        playerScore++;
+        incrementPlayerScore();
      }
      else if (playerOption == '0' && computerOption == '1') {
         resultMessage.innerHTML = "You Lose! Lets have another round!";
-        computerScore++;
+        incrementComputerScore();
      }
      else if (playerOption == '1' && computerOption == '0') {
         resultMessage.innerHTML = "You Win! Lets have another round!";
-        playerScore++;
+        incrementPlayerScore();
      }     
      else if (playerOption == '1' && computerOption == '2') {
         resultMessage.innerHTML = "You Lose! Lets have another round!";
-        computerScore++;
+        incrementComputerScore();
      }
      else if (playerOption == '2' && computerOption == '1') {
         resultMessage.innerHTML = "You Win! Lets have another round!";
-        playerScore++;
+        incrementPlayerScore();
      } 
      else if (playerOption == '2' && computerOption == '0') {
         resultMessage.innerHTML = "You Lose! Lets have another round!";
-        computerScore++;
+        incrementComputerScore();
      } 
      else {
         resultMessage.innerHTML = "Its a draw! Lets have another round!";
      }
  }
 
-function updateScores() {
 
+// Increment scores functions based on Code Institute's 'Love Maths' score function code
+/**
+ * the functions below extract the current scores for the player and computer and then increment by 1.
+ * these functions are called in the checkWinner function above
+ */
+function incrementPlayerScore() {
+   let oldScore = parseInt(document.getElementById("player-score").innerText);
+   document.getElementById("player-score").innerText = ++oldScore;
+}
+
+function incrementComputerScore() {
+   let oldScore = parseInt(document.getElementById("computer-score").innerText);
+   document.getElementById("computer-score").innerText = ++oldScore;
 }
